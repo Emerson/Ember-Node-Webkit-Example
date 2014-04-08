71e2cfd887365de935b39b320f468f2442c51fcc
Emerson Lackey <emerson.lackey@gmail.com>
2014-04-08 11:02:35 -0400

    Adding a basic acceptance test

---
+++ tests/acceptance/basic-test.js
@@ -0,0 +1,18 @@
+import startApp from './../helpers/start-app';
+var App;
+
+module('Acceptance - Basic', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('it should initialize', function() {
+  expect(1);
+  visit('/').then(function() {
+    ok($(App.rootElement).hasClass('ember-application'), 'The application was not initialized');
+  });
+});
