dfd261960a077e8e615b6699c577994a7480876b
Emerson Lackey <emerson.lackey@gmail.com>
2014-04-07 21:51:04 -0400

    added a shim that allows both types of module requires

--- utils/node-webkit-shim.js
+++ utils/node-webkit-shim.js
@@ -1,5 +1,13 @@
 if (window.nwDispatcher) {
-  // window.win = require('nw.gui').Window.get();
   window.node_require = window.require;
   window.require = undefined;
 }
+
+// Live reload equivlent
+var fs = node_require('fs');
+console.log('now watching tmp/restart.txt');
+fs.watchFile('./tmp/restart.txt', [], function() {
+  if (location) {
+    location.reload(false);
+  }
+});
